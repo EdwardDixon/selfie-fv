8f87b1061a6d9492e7b50dbe39af7595844df958
Edward Dixon <dixon.edward@gmail.com>
2020-11-04 17:22:33 +0000

    Now storing data on GCP to reduce GitHub data costs

---
+++ data/fetch_data.sh
@@ -0,0 +1,3 @@
+wget -nc https://storage.googleapis.com/selfie-fv/selfies.zip
+wget -nc https://storage.googleapis.com/selfie-fv/selfie_train.pkl
+wget -nc https://storage.googleapis.com/selfie-fv/selfie_test.pkl
